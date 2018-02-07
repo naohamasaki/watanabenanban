c95e371cc16239ef87a6b6bb237fc6137e5e4154
naohamasaki <naohamasaki1988@gmail.com>
2018-02-07 18:01:51 +0900

    watanabenanban02
    This reverts commit 9c1180879133e33cad4dfa4a0bbbe3f7fbadb618.

--- js/swiper_custom.js
+++ js/swiper_custom.js
@@ -1,15 +1,17 @@
 var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        slidesPerView: 2,
-        spaceBetween: 10,
-        centeredSlides : true,
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        breakpoints: {
-            767: {
-                slidesPerView: 1,
-                spaceBetween: 0
-            }
+    loop: true,
+    slidesPerView: 2,
+    //spaceBetween: 10,
+    autoplay:3000,
+    speed: 2000,
+    centeredSlides : true,
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+        767: {
+            slidesPerView: 1,
+            spaceBetween: 0
         }
-    })
+    }
+})
